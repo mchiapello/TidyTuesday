e442013f2f96774ba459f12b1e6aa17bf5ab7ad4
Marco Chiapello <chiapello.m@gmail.com>
2021-12-13 13:13:19 +0100

    Update folder name

--- master.R
+++ master.R
@@ -1,10 +1,11 @@
 # Which week do you want to analyze?
 day <- '2021-12-07'
+year <- 2021
 week <- lubridate::week(lubridate::ymd(day))    
 
 # Create the analysis folder
-if(!dir.exists(here::here(paste0("week", week)))){
-    fs::dir_create(paste0("week", week))
+if(!dir.exists(here::here(paste0(year, "_week", week)))){
+    fs::dir_create(paste0(year, "_week", week))
 } else {
     message("This week folder already exist!")
 }
@@ -13,8 +14,8 @@ if(!dir.exists(here::here(paste0("week", week)))){
 tuesdata <- tidytuesdayR::tt_load(day)
 
 # Create backbone script
-if (!file.exists(paste0("week", week, "/", day, ".R"))){
-    fs::file_copy("backbone.R", paste0("week", week, "/", day, ".R"))
+if (!file.exists(paste0(year, "_week", week, "/", day, ".R"))){
+    fs::file_copy("backbone.R", paste0(year, "_week", week, "/", day, ".R"))
 } else {
     message("This file already exist!")
 }
